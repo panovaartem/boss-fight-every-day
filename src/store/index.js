9d1e8dada9159550a9d06c3a2de8408fbc10b078
Artem Panov <43380883+panovaartem@users.noreply.github.com>
2024-09-10 11:05:55 +0500

    init

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment(state) {
+      state.count++
+    }
+  },
+  actions: {
+    increment({ commit }) {
+      commit('increment')
+    }
+  },
+  getters: {
+    count: state => state.count
+  },
+  modules: {}
+})
